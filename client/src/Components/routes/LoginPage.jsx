1f3bd8fad4fafa7bd6dc1d5c60b5517440d1e57e
Tamas Nemeth <nemeth.t91@gmail.com>
2024-07-20 19:26:15 +0100

    add login form

--- client/src/Components/routes/LoginPage.jsx
+++ client/src/Components/routes/LoginPage.jsx
@@ -1,10 +1,38 @@
 import React from "react";
+import InputBox from "../Form/Inputbox";
+import { useState } from "react";
 
 function LogInPage(){
 
+    const [user, setUser] = useState({
+        email: "",
+        password: ""
+    });
+
+    function updateUser(inputName, newValue){
+        setUser((preValue) =>{
+            return{
+                ...preValue,
+                [inputName]: newValue
+            }
+        })
+    }
+
     return <div className="login page">
-        <h1>This is the Log In page</h1>
+        <div className="card">
+            <h2>Create Acount</h2>
+            <form >
+                <InputBox name="email" label="Email" updateField={updateUser} value={user.email}/>
+                <InputBox name="password" label="Password" updateField={updateUser} value={user.password}/>
+                <button type="submit" className="btn">Sign In</button>
+                <div class="divider">
+                    <p >or</p>
+                </div>
+                <button type="submit" className="btn">Create an Account</button>
+              </form>
+        </div>
     </div>
 }
 
 export default LogInPage;
+
