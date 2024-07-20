bee7ab4fb750317697b7bc0ebcf5141ac5a68c0b
Tamas Nemeth <nemeth.t91@gmail.com>
2024-07-20 15:03:32 +0100

    rename landing page

--- client/src/Components/routes/LandingPage.jsx
+++ client/src/Components/routes/LandingPage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function LandingPage(){
 
     return <div className="landing page">
-        <h1>This is the Log In page</h1>
+        <h1>This is the Landing page</h1>
     </div>
 }
 
