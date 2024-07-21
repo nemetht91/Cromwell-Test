86ca77b6d12bb28cfa46f5c278643c80a3b07757
Tamas Nemeth <nemeth.t91@gmail.com>
2024-07-21 11:32:25 +0100

    add usercontext and login process

--- client/src/Components/routes/LoginPage.jsx
+++ client/src/Components/routes/LoginPage.jsx
@@ -2,18 +2,21 @@ import React from "react";
 import InputBox from "../Form/Inputbox";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "../UserContext";
 
 function LogInPage(){
 
-    const [user, setUser] = useState({
+    const [data, setData] = useState({
         email: "",
         password: ""
     });
 
     const navigate = useNavigate();
+    const user = useContext(UserContext);
 
     function updateUser(inputName, newValue){
-        setUser((preValue) =>{
+        setData((preValue) =>{
             return{
                 ...preValue,
                 [inputName]: newValue
@@ -21,13 +24,44 @@ function LogInPage(){
         })
     }
 
+    function handleClick(event){
+        const logIn = async () => {
+            const baseURL = process.env.REACT_APP_API_SERVER_URL;
+            console.log(baseURL)
+            try {
+                const response = await fetch(baseURL+"/user/login?"+ new URLSearchParams({
+                    email: data.email,
+                    password: data.password
+                }),{
+                    method: 'POST',
+    
+                });
+                if(response.status == 200){
+                    const userData = await response.json();
+                    user.logInUser(userData.email, userData.firstName, userData.lastName);
+                    navigate('/landing')
+                }
+                else{
+                    const error = await response.json();
+                    console.log(error);
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        event.preventDefault();
+        logIn();
+    }
+
+
+
     return <div className="login page">
         <div className="card">
             <h2>Create Acount</h2>
             <form >
-                <InputBox name="email" label="Email" type="text" updateField={updateUser} value={user.email}/>
-                <InputBox name="password" label="Password" type="password" updateField={updateUser} value={user.password}/>
-                <button type="submit" className="btn">Sign In</button>
+                <InputBox name="email" label="Email" type="text" updateField={updateUser} value={data.email}/>
+                <InputBox name="password" label="Password" type="password" updateField={updateUser} value={data.password}/>
+                <button onClick={handleClick} type="submit" className="btn">Sign In</button>
                 <div class="divider">
                     <p >or</p>
                 </div>
