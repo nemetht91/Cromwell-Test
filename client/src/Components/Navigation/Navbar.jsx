3faaae6d4e5a5194aabfafeefb054c835c72695c
Tamas Nemeth <nemeth.t91@gmail.com>
2024-07-21 11:31:52 +0100

    add usercontext

--- client/src/Components/Navigation/Navbar.jsx
+++ client/src/Components/Navigation/Navbar.jsx
@@ -2,17 +2,27 @@ import React from "react";
 import Logo from "./logo";
 import NavButton from "./NavButton";
 import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "../UserContext";
 
 function Navbar(){
 
-
     const navigate = useNavigate();
+    const user = useContext(UserContext);
+
+    function logOut(){
+        user.logOutUser();
+        navigate('/');
+    }
 
     return <div className="navbar">
         <div className="container">
             <Logo navigate={() => {navigate('/')}}/>
             <div className="buttons">
-                <NavButton navigate={() => {navigate('/login')}} name="Sign In"/>
+                {user.isLogedIn && <NavButton navigate={() => {navigate('/landing')}} name={user.getName()}/>}
+                {user.isLogedIn?
+                <NavButton navigate={logOut} name="Log Out"/>:
+                <NavButton navigate={() => {navigate('/login')}} name="Sign In"/>}                
             </div>
         </div>
     </div>
