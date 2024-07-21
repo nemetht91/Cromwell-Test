6b8d1bbf74ed84e2514e8182116152f70e6cd69f
Tamas Nemeth <nemeth.t91@gmail.com>
2024-07-21 11:32:03 +0100

    add usercontext

--- client/src/Components/routes/LandingPage.jsx
+++ client/src/Components/routes/LandingPage.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { UserContext } from "../UserContext";
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function LandingPage(){
 
+    const user = useContext(UserContext);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(!user.isLogedIn){
+            navigate('/');
+        }
+    }, [])
+
     return <div className="landing page">
         <h1>This is the Landing page</h1>
+        <h2>{user.getName()}</h2>
+        <h2>{user.user.email}</h2>
     </div>
 }
 
