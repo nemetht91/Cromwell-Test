97be3cd163c764c062ca2e7276246ba643486d7a
Tamas Nemeth <nemeth.t91@gmail.com>
2024-07-21 13:47:39 +0100

    style homepage

--- client/src/Components/routes/HomePage.jsx
+++ client/src/Components/routes/HomePage.jsx
@@ -3,7 +3,9 @@ import React from "react";
 function HomePage(){
 
     return <div className="home page">
-        <h1>This is the home page</h1>
+        <h1>This is a test project</h1>
+        <h2>For Junior Full Stack Developer position</h2>
+        <h2>at <a href="https://www.cromwell.co.uk/" target="blank">Cromwell</a></h2>
     </div>
 }
 
