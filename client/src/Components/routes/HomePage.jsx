5af52134934bf6182e8b1694bc1716ef879e945c
Tamas Nemeth <nemeth.t91@gmail.com>
2024-07-21 16:45:32 +0100

    add image reference

--- client/src/Components/routes/HomePage.jsx
+++ client/src/Components/routes/HomePage.jsx
@@ -6,6 +6,7 @@ function HomePage(){
         <h1>This is a test project</h1>
         <h2>For Junior Full Stack Developer position</h2>
         <h2>at <a href="https://www.cromwell.co.uk/" target="blank">Cromwell</a></h2>
+        <a className="image_ref" target="blank" href="https://www.freepik.com/free-photo/view-steel-hammer-with-other-construction-elements-tools_49590461.htm#fromView=search&page=1&position=29&uuid=529fe204-c149-4ca2-ad5f-ee699f7d8c67">Image by freepik</a>
     </div>
 }
 
